9c6d1f6ee3fba4c784867dbe2fc177bfd7b70438
Leonardo Guimel <85853201+CharlizeWhite@users.noreply.github.com>
2024-02-03 19:29:38 -0600

    modificacion de script
    mejore identacion, mejora codigo, mejore funciones y quedo en total con 62 lineas de codigo

--- script/app.js
+++ script/app.js
@@ -1,81 +1,62 @@
-function  botonEncriptar(){   
-    let captura = document.getElementById('inputText').value;
-    //Validación del texto ingresado
-    let resultado = [];
-    let palabras = captura.split("");
-        for (let palabra of palabras){
-            if(/[A-Z]/.test(palabra)){
-                resultado.push(palabra);
-                console.log(resultado);
-                alert("Se han encontrado palabras con mayúsculas");
-                throw new Error("Se han encontrado palabras con mayúsculas");
-                
-            }
-        }
-        let resultado2 = [];
-        let acentos = captura.split("");
-        for (let acento of acentos){
-            if(/[^a-z^\s]/.test(acento)){
-                resultado2.push(acento);
-                console.log(resultado2);
-                alert("Se han encontrado palabras con acentos, simbolos o números");
-                throw new Error("Se han encontrado palabras con acentos, símbolos o números");
-                
-            }
-        }
-    //¡Inicia Encriptación!    
-    let encriptar = captura.replace(/e/g,'enter').replace(/i/g,'imes').replace(/a/g,'ai').replace(/o/g,'ober').replace(/u/g,'ufat').replace(/y/g,'imeyes');
-    
-    document.getElementById('desencriptar').removeAttribute('hidden');
-    document.getElementById('outputText').textContent = encriptar;
-    document.querySelector('#sinMensaje').setAttribute('hidden',true); 
-    document.querySelector('#imagenBusqueda').setAttribute('hidden',true);
-    document.querySelector('#imagenConLupa').setAttribute('hidden',true);
-    //Validación en caso de casilla en blanco    
+function validacion(){ //Validación de espacio en blanco
     if (captura === ""){
-            alert("Ingresa Texto a Encriptar");
-            limpiarTexto();
-    }
-    }
-
-
-
-    
-
-function botonDesencriptar(){
-    let captura = document.getElementById('inputText').value;
-    let desencriptar = captura.replace(/enter/g,'e').replace(/imes/g,'i').replace(/ai/g,'a').replace(/ober/g,'o').replace(/ufat/g,'u').replace(/imeyes/g,'y');
-    document.getElementById('desencriptar').removeAttribute('hidden');
-    document.getElementById('outputText').textContent = desencriptar;
-    document.querySelector('#sinMensaje').setAttribute('hidden',true);
-    document.querySelector('#imagenBusqueda').setAttribute('hidden',true);
-    document.querySelector('#imagenConLupa').setAttribute('hidden',true);
-        if (captura === ""){
-            alert("Ingresa Texto Encriptado");
-            limpiarTexto();
-        }
+        alert("Ingresa Texto a Encriptar/Desencriptar");
+        throw new Error("casilla en blanco");
+    }else{    
+        let resultado = [];//Validación de mayúsculas
+        let palabras = captura.split("");
+            for (let palabra of palabras){
+                if(/[A-Z]/.test(palabra)){
+                    resultado.push(palabra);
+                    console.log(resultado);
+                    alert("Se han encontrado palabras con mayúsculas");
+                    throw new Error("Se han encontrado palabras con mayúsculas");
+                }
+            }
+        let resultado2 = [];//Validación de acentos, simbolos o números
+        let acentos = captura.split("");
+            for (let acento of acentos){
+                if(/[^a-z^¡!^\s]/.test(acento)){
+                    resultado2.push(acento);
+                    console.log(resultado2);
+                    alert("Se han encontrado palabras con acentos, simbolos o números");
+                    throw new Error("Se han encontrado palabras con acentos, símbolos o números");
+                }
+            }
+    }     
 }
-
-function copiarTexto(){
+function  botonEncriptar(){   //¡Inicia Encriptación!    
+    let = captura = document.getElementById('inputText').value;
+    validacion(); //Inicia validación
+    let encriptar = captura.replace(/e/g,'enter').replace(/i/g,'imes').replace(/a/g,'ai').replace(/o/g,'ober').replace(/u/g,'ufat').replace(/y/g,'imeyes');
+    document.getElementById('outputText').textContent = encriptar;
+    cambioAtributos();//Cambios en codigo HTML 
+}
+function cambioAtributos(){ //Cambio de atributos en etiquetas
+        document.getElementById('desencriptar').removeAttribute('hidden');
+        document.querySelector('#sinMensaje').setAttribute('hidden',true);
+        document.querySelector('#imagenBusqueda').setAttribute('hidden',true);
+        document.querySelector('#imagenConLupa').setAttribute('hidden',true);
+}
+function botonDesencriptar(){ //¡Inicia Desencriptación!
+    let captura = document.getElementById('inputText').value;
+    validacion(); //También debe tener validación
+    let desencriptar = captura.replace(/enter/g,'e').replace(/imes/g,'i').replace(/ai/g,'a').replace(/ober/g,'o').replace(/ufat/g,'u').replace(/imeyes/g,'y');
+    document.getElementById('outputText').textContent = desencriptar;
+    cambioAtributos(); 
+}
+function copiarTexto(){ // Botón copiar texto
     let outputText = document.getElementById('outputText');
-    let textoModificado = outputText.textContent;
-    
     outputText.select();
     outputText.setSelectionRange(0,99999);
-
     navigator.clipboard.writeText(outputText.value);
-
-  
-    
     limpiarTexto();
 }
-
-function limpiarTexto(){
+function limpiarTexto(){ //Función limpiar área de texto
     document.getElementById('outputText').textContent='';
     document.getElementById('inputText').value='';
     document.querySelector('#desencriptar').setAttribute('hidden',true);
     document.getElementById('sinMensaje').removeAttribute('hidden');
     document.getElementById('imagenBusqueda').removeAttribute('hidden');
     document.getElementById('imagenConLupa').removeAttribute('hidden');
-    
 }
