669537390bb6da6b66ce4cc1fe891241a1c5980b
Leonardo Guimel <85853201+CharlizeWhite@users.noreply.github.com>
2024-02-03 21:22:03 -0600

    modificacion codigo script
    la función que valida el texto ahora tiene un operardor para el dato undefined

--- script/app.js
+++ script/app.js
@@ -1,5 +1,21 @@
-function validacion(){ //Validación de espacio en blanco
-    if (captura === ""){
+function  botonEncriptar(){   //¡Inicia Encriptación!    
+    let = captura = document.getElementById('inputText').value;
+    validacion(); //Inicia validación
+    let encriptar = captura.replace(/e/g,'enter').replace(/i/g,'imes').replace(/a/g,'ai').replace(/o/g,'ober').replace(/u/g,'ufat').replace(/y/g,'imeyes');
+    document.getElementById('outputText').textContent = encriptar;
+    cambioAtributos();//Cambios en codigo HTML 
+}
+
+function botonDesencriptar(){ //¡Inicia Desencriptación!
+    let captura = document.getElementById('inputText').value;
+    validacion(); //También debe tener validación
+    let desencriptar = captura.replace(/enter/g,'e').replace(/imes/g,'i').replace(/ai/g,'a').replace(/ober/g,'o').replace(/ufat/g,'u').replace(/imeyes/g,'y');
+    document.getElementById('outputText').textContent = desencriptar;
+    cambioAtributos(); 
+}
+
+function validacion(){ //Validacion de texto capturado
+    if (captura === "" || undefined){ //Validación de espacio en blanco o no definido
         alert("Ingresa Texto a Encriptar/Desencriptar");
         throw new Error("casilla en blanco");
     }else{    
@@ -13,7 +29,6 @@ function validacion(){ //Validación de espacio en blanco
                     throw new Error("Se han encontrado palabras con mayúsculas");
                 }
             }
-        
         let resultado2 = [];//Validación de acentos, simbolos o números
         let acentos = captura.split("");
             for (let acento of acentos){
@@ -27,27 +42,11 @@ function validacion(){ //Validación de espacio en blanco
     }     
 }
 
-function  botonEncriptar(){   //¡Inicia Encriptación!    
-    let = captura = document.getElementById('inputText').value;
-    validacion(); //Inicia validación
-    let encriptar = captura.replace(/e/g,'enter').replace(/i/g,'imes').replace(/a/g,'ai').replace(/o/g,'ober').replace(/u/g,'ufat').replace(/y/g,'imeyes');
-    document.getElementById('outputText').textContent = encriptar;
-    cambioAtributos();//Cambios en codigo HTML 
-}
-
 function cambioAtributos(){ //Cambio de atributos en etiquetas
-        document.getElementById('desencriptar').removeAttribute('hidden');
-        document.querySelector('#sinMensaje').setAttribute('hidden',true);
-        document.querySelector('#imagenBusqueda').setAttribute('hidden',true);
-        document.querySelector('#imagenConLupa').setAttribute('hidden',true);
-}
-
-function botonDesencriptar(){ //¡Inicia Desencriptación!
-    let captura = document.getElementById('inputText').value;
-    validacion(); //También debe tener validación
-    let desencriptar = captura.replace(/enter/g,'e').replace(/imes/g,'i').replace(/ai/g,'a').replace(/ober/g,'o').replace(/ufat/g,'u').replace(/imeyes/g,'y');
-    document.getElementById('outputText').textContent = desencriptar;
-    cambioAtributos(); 
+    document.getElementById('desencriptar').removeAttribute('hidden');
+    document.querySelector('#sinMensaje').setAttribute('hidden',true);
+    document.querySelector('#imagenBusqueda').setAttribute('hidden',true);
+    document.querySelector('#imagenConLupa').setAttribute('hidden',true);
 }
 
 function copiarTexto(){ // Botón copiar texto
